13c811732252852bde4cd03b2dd945b7761a7c06
SuRu <33urus@gmail.com>
2021-03-02 18:23:40 +0200

    added test profile

--- src/test/java/com/suru/usersapi/SmokeTest.java
+++ src/test/java/com/suru/usersapi/SmokeTest.java
@@ -4,10 +4,12 @@ import com.suru.usersapi.controllers.UserController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("TEST")
 public class SmokeTest {
 
     @Autowired
